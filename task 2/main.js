aff8fed86b40bb90fd73e36e72e6de9488b1acb7
SuhaniTandel <tandelsuhani1608@gmail.com>
2024-12-17 13:37:40 +0530

    add file

---
+++ task 2/main.js
@@ -0,0 +1,8 @@
+let name = "Suhani Tandel"
+let exp1 = "I make a Web project on EV charging locator and slot booking website"
+let exp2 = "We make a final year project on the cleanerside webapp"
+
+
+document.getElementById("myH1").textContent = `My Name is ${name}`;
+document.getElementById("ex1").textContent = `I previosuly worked on ${exp1}`;
+document.getElementById("ex2").textContent = `Before that ${exp2}`;
